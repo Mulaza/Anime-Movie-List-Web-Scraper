665638ee33acfd2e21caafef2e0230da632c9003
Mulaza <mulazajacinto@Gmail.com>
2020-09-23 17:59:05 +0800

    Initial Commit

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
+# ------------------------------------------------------------------------------------------------------------------
+# NOTE: This only works under the assumption that the accessed web page does not change its layout and class naming
+# ------------------------------------------------------------------------------------------------------------------
+
+# Need to be installed with pip
 from bs4 import BeautifulSoup
 import requests
-# url = "https://www.tutorialspoint.com/index.htm"
-# req = requests.get(url)
-# soup = BeautifulSoup(req.text, "html.parser")
-# print(soup.title)
-#
-#
-# for link in soup.find_all('a'):
-#     print(link.get('href'))
 
-#-------------------------------------------
-url = "https://www.theverge.com/2020/9/13/21435507/nvidia-acquiring-arm-40-billion-chips-ai-deal"
+# ------------------------------------------------------------------------------------------------------------------
+
+# Go to imdb 150 top Anime movie list page
+url = "https://www.imdb.com/list/ls026128329/"
 req = requests.get(url)
 soup = BeautifulSoup(req.text, "html.parser")
-print(soup, "\n")
 
-print("\n", soup.h1.text, "\n")
+# look fot only the 'h3' tage with the class of 'lister-item-header'
+tags = soup.findAll("h3", class_="lister-item-header")
 
-for a in soup.find_all('p'):
-    print(a)
+
+# create an empty string to hold the movie names
+listString = ""
+
+
+# loop through the h3 tags to get a list of a tags and het the text within them
+for a in tags:
+
+    # append every movie name to the end of a list
+    listString = listString + "{} \n".format(a.find_all('a')[0].text)
+
+
+# create a file to add and hold all of the movie names
+with open("Anime-Movie-list.txt", 'a') as fileObject:
+    fileObject.write(listString)
 
